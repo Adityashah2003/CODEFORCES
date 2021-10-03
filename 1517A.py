02f3784c7d58c9702a4df65f0f02524397d68b20
Aditya <84448014+Flamethrowerp@users.noreply.github.com>
2021-10-03 12:29:46 +0530

    Add files via upload

---
+++ 1517A.py
@@ -0,0 +1,14 @@
+n=int(input())
+list = []
+while n!=0:
+  number = int(input())
+  buffer= number/2050
+  if number%2050 !=0:
+    print("-1")
+  else:
+    sum=0
+    while buffer!=0:
+      sum = sum +int(buffer%10)
+      buffer = int(buffer/10)
+    print(sum)
+  n=n-1
